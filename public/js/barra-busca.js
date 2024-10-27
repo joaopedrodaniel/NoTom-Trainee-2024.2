faf810a22b992c7dfa5d3f93266f8f1c36f8ae75
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-10-27 16:35:05 -0300

    Adiciona mudança de visibilidade de ícone de cancelar busca

--- public/js/barra-busca.js
+++ public/js/barra-busca.js
@@ -1,5 +1,12 @@
 let ultimoScrollY = window.scrollY;
 const barraBusca = document.querySelector(".barra-busca-container");
+const iconeLupa = document.querySelector(
+  '.barra-busca ion-icon[name="search-outline"]'
+);
+const iconeX = document.querySelector(
+  '.barra-busca ion-icon[name="close-outline"]'
+);
+const buscaInput = barraBusca.querySelector("#busca-input");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > ultimoScrollY) {
@@ -8,4 +15,15 @@ window.addEventListener("scroll", () => {
     barraBusca.classList.remove("barra-busca-escondida");
   }
   ultimoScrollY = window.scrollY;
+});
+
+iconeLupa.addEventListener("click", () => {
+  iconeX.classList.add("icone-ativo");
+});
+
+iconeX.addEventListener("click", () => {
+  if (iconeX.classList.contains("icone-ativo")) {
+    iconeX.classList.remove("icone-ativo");
+    buscaInput.value = "";
+  }
 });
