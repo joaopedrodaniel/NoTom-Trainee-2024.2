592eeb4dae278b38143322b07cb0d712a8b7093b
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-10-29 17:18:56 -0300

    Adiciona título à pagina de posts

--- public/js/barra-busca.js
+++ public/js/barra-busca.js
@@ -1,18 +1,43 @@
 let ultimoScrollY = window.scrollY;
-const barraBusca = document.querySelector(".barra-busca-container");
+const titulo = document.querySelector(".titulo");
+const barraBuscaContainer = document.querySelector(".barra-busca-container");
 const iconeLupa = document.querySelector(
   '.barra-busca ion-icon[name="search-outline"]'
 );
 const iconeX = document.querySelector(
   '.barra-busca ion-icon[name="close-outline"]'
 );
-const buscaInput = barraBusca.querySelector("#busca-input");
+const buscaInput = barraBuscaContainer.querySelector("#busca-input");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > ultimoScrollY) {
-    barraBusca.classList.add("barra-busca-escondida");
+  console.log(window.scrollY);
+  if (window.scrollY >= 135) {
+    barraBuscaContainer.classList.remove(
+      "barra-busca-container-com-espaco-para-titulo"
+    );
+    barraBuscaContainer.classList.add("barra-busca-container-fixa");
+    titulo.classList.add("barra-busca-container-fixa-titulo");
   } else {
-    barraBusca.classList.remove("barra-busca-escondida");
+    if (
+      window.scrollY < 90 &&
+      barraBuscaContainer.classList.contains("barra-busca-container-fixa")
+    ) {
+      barraBuscaContainer.classList.add(
+        "barra-busca-container-com-espaco-para-titulo"
+      );
+    }
+    if (window.scrollY < 55) {
+      barraBuscaContainer.classList.remove(
+        "barra-busca-container-com-espaco-para-titulo"
+      );
+      barraBuscaContainer.classList.remove("barra-busca-container-fixa");
+      titulo.classList.remove("barra-busca-container-fixa-titulo");
+    }
+  }
+  if (window.scrollY > ultimoScrollY) {
+    barraBuscaContainer.classList.add("barra-busca-escondida");
+  } else {
+    barraBuscaContainer.classList.remove("barra-busca-escondida");
   }
   ultimoScrollY = window.scrollY;
 });
