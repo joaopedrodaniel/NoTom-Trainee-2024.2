dc1f1a4a22676a94a1ecf0660417f33ca8df3557
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-11-05 18:09:23 -0300

    Adiciona segundo nível de responsividade

--- public/js/lista-usuarios.js
+++ public/js/lista-usuarios.js
@@ -21,12 +21,21 @@ const mudarChevron = (accordion) => {
 };
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
+    if (!table.classList.contains("responsividade-1")) {
+      return;
+    }
     mudarChevron(accordion);
     if (table.classList.contains("responsividade-1")) {
       accordion.classList.toggle("open-accordion");
-      const panel = accordion.nextElementSibling;
-      panel.classList.toggle("hidden-content");
-      panel.classList.toggle("display-content");
+      const panel1 = accordion.nextElementSibling;
+      const panel2 = panel1.nextElementSibling;
+      if (table.classList.contains("responsividade-2")) {
+        panel1.classList.toggle("hidden-content");
+        panel1.classList.toggle("display-content");
+      }
+
+      panel2.classList.toggle("hidden-content");
+      panel2.classList.toggle("display-content");
     }
   });
 });
@@ -36,7 +45,17 @@ const checarSeTableTransborda = () => {
     tableContainer.scrollWidth > tableContainer.clientWidth;
   console.log(hasHorizontalScroll);
   console.log(tableContainer.clientWidth, tableContainer.scrollWidth);
-  if (!hasHorizontalScroll) {
+  if (!hasHorizontalScroll && table.classList.contains("responsividade-2")) {
+    table.classList.remove("responsividade-2");
+    if (tableContainer.scrollWidth > tableContainer.clientWidth) {
+      table.classList.add("responsividade-2");
+    } else {
+      table.querySelectorAll(".linha-email").forEach((tr) => {
+        tr.classList.add("hidden-content");
+      });
+    }
+  }
+  if (!hasHorizontalScroll && !table.classList.contains("responsividade-2")) {
     table.classList.remove("responsividade-1");
 
     table.querySelectorAll(".celula-acoes").forEach((celula) => {
@@ -53,18 +72,23 @@ const checarSeTableTransborda = () => {
       table.querySelectorAll(".celula-icone.hidden").forEach((celula) => {
         celula.classList.remove("hidden");
       });
+    } else {
       accordions.forEach((accordion) => {
         if (accordion.classList.contains("open-accordion")) {
           mudarChevron(accordion);
         }
+        accordion.classList.remove("open-accordion");
+        const panel1 = accordion.nextElementSibling;
+        const panel2 = panel1.nextElementSibling;
+        if (table.classList.contains("responsividade-2")) {
+          panel1.classList.add("hidden-content");
+          panel1.classList.remove("display-content");
+        }
+
+        panel2.classList.add("hidden-content");
+        panel2.classList.remove("display-content");
       });
     }
-    accordions.forEach((accordion) => {
-      accordion.classList.remove("open-accordion");
-      const panel = accordion.nextElementSibling;
-      panel.classList.add("hidden-content");
-      panel.classList.remove("display-content");
-    });
   }
   if (hasHorizontalScroll && !table.classList.contains("reponsividade-1")) {
     table.classList.add("responsividade-1");
@@ -73,6 +97,19 @@ const checarSeTableTransborda = () => {
     });
     table.querySelectorAll(".celula-icone.hidden").forEach((celula) => {
       celula.classList.remove("hidden");
+    });
+  }
+  if (
+    tableContainer.scrollWidth > tableContainer.clientWidth &&
+    table.classList.contains("responsividade-1")
+  ) {
+    table.classList.add("responsividade-2");
+    accordions.forEach((accordion) => {
+      const panel1 = accordion.nextElementSibling;
+      if (accordion.classList.contains("open-accordion")) {
+        panel1.classList.remove("hidden-content");
+        panel1.classList.add("display-content");
+      }
     });
   }
 };
