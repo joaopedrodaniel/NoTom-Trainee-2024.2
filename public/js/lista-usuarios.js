66045dc5569db8731d24d88d612b0492680fb457
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-11-05 17:17:55 -0300

    Adiciona primeiro nível de responsividade à tabela

--- public/js/lista-usuarios.js
+++ public/js/lista-usuarios.js
@@ -1,26 +1,33 @@
 const accordions = document.querySelectorAll(".accordion");
 const tableContainer = document.querySelector(".table-container");
+const table = tableContainer.querySelector("table");
+
+const mudarChevron = (accordion) => {
+  if (accordion.classList.contains("open-accordion")) {
+    const cell = accordion.querySelector(".celula-icone");
+    const cellDiv = cell.querySelector(".center-div");
+    cellDiv.innerHTML = "";
+    const icon = document.createElement("ion-icon");
+    icon.setAttribute("name", "chevron-down-outline");
+    cellDiv.appendChild(icon);
+  } else {
+    const cell = accordion.querySelector(".celula-icone");
+    const cellDiv = cell.querySelector(".center-div");
+    cellDiv.innerHTML = "";
+    const icon = document.createElement("ion-icon");
+    icon.setAttribute("name", "chevron-up-outline");
+    cellDiv.appendChild(icon);
+  }
+};
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
-    if (accordion.classList.contains("open-accordion")) {
-      const cell = accordion.querySelector(".celula-icone");
-      const cellDiv = cell.querySelector(".center-div");
-      cellDiv.innerHTML = "";
-      const icon = document.createElement("ion-icon");
-      icon.setAttribute("name", "chevron-down-outline");
-      cellDiv.appendChild(icon);
-    } else {
-      const cell = accordion.querySelector(".celula-icone");
-      const cellDiv = cell.querySelector(".center-div");
-      cellDiv.innerHTML = "";
-      const icon = document.createElement("ion-icon");
-      icon.setAttribute("name", "chevron-up-outline");
-      cellDiv.appendChild(icon);
+    mudarChevron(accordion);
+    if (table.classList.contains("responsividade-1")) {
+      accordion.classList.toggle("open-accordion");
+      const panel = accordion.nextElementSibling;
+      panel.classList.toggle("hidden-content");
+      panel.classList.toggle("display-content");
     }
-    accordion.classList.toggle("open-accordion");
-    const panel = accordion.nextElementSibling;
-    panel.classList.toggle("hidden-content");
-    panel.classList.toggle("display-content");
   });
 });
 
@@ -28,8 +35,48 @@ const checarSeTableTransborda = () => {
   const hasHorizontalScroll =
     tableContainer.scrollWidth > tableContainer.clientWidth;
   console.log(hasHorizontalScroll);
+  console.log(tableContainer.clientWidth, tableContainer.scrollWidth);
+  if (!hasHorizontalScroll) {
+    table.classList.remove("responsividade-1");
+
+    table.querySelectorAll(".celula-acoes").forEach((celula) => {
+      celula.classList.remove("hidden");
+    });
+    table.querySelectorAll(".celula-icone").forEach((celula) => {
+      celula.classList.add("hidden");
+    });
+    if (tableContainer.scrollWidth > tableContainer.clientWidth) {
+      table.classList.add("responsividade-1");
+      table.querySelectorAll(".celula-acoes").forEach((celula) => {
+        celula.classList.add("hidden");
+      });
+      table.querySelectorAll(".celula-icone.hidden").forEach((celula) => {
+        celula.classList.remove("hidden");
+      });
+      accordions.forEach((accordion) => {
+        if (accordion.classList.contains("open-accordion")) {
+          mudarChevron(accordion);
+        }
+      });
+    }
+    accordions.forEach((accordion) => {
+      accordion.classList.remove("open-accordion");
+      const panel = accordion.nextElementSibling;
+      panel.classList.add("hidden-content");
+      panel.classList.remove("display-content");
+    });
+  }
+  if (hasHorizontalScroll && !table.classList.contains("reponsividade-1")) {
+    table.classList.add("responsividade-1");
+    table.querySelectorAll(".celula-acoes").forEach((celula) => {
+      celula.classList.add("hidden");
+    });
+    table.querySelectorAll(".celula-icone.hidden").forEach((celula) => {
+      celula.classList.remove("hidden");
+    });
+  }
 };
 
-window.addEventListener("resize", checarSeTableTransborda);
-
-checarSeTableTransborda();
+window.addEventListener("resize", () => {
+  checarSeTableTransborda();
+});
