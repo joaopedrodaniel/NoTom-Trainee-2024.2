c49c290dd30d376ab35ba5677d92cb40d47976c1
lucas <lucasferoliveira.goval@gmail.com>
2024-11-27 01:31:59 -0300

    Começa processo de adicionar a opção de Reset Password na Login Page, branch não obrigatoria, irei tentar terminar ela para entregar junto com o projeto

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+function abirModal(idModal) {
+    document.getElementById(idModal).style.display = "block";
+  }
+  
+  function fecharModal(idModal) {
+    document.getElementById(idModal).style.display = "none";
+  }
+
