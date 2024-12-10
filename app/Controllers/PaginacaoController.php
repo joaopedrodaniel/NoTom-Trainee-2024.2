33010063a1b1dbb4d11bfd6e9efb435bf7db78bd
lucas <lucasferoliveira.goval@gmail.com>
2024-12-10 03:22:19 -0300

    Corrige ultimos detalhes para merge com main

--- app/Controllers/PaginacaoController.php
+++ app/Controllers/PaginacaoController.php
@@ -19,7 +19,7 @@ class PaginacaoController
             }
         }
 
-        $itensPage = 1;
+        $itensPage = 8;
         $inicio = $itensPage * $page - $itensPage;
         $rows_count = App::get('database')->countAll('posts');
 
