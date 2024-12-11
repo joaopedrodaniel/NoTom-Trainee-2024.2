8e9099e0e59bd79e33b9e498e7834f9ec09a9d14
davigrossis <davi.grossi.souza@gmail.com>
2024-12-11 15:13:49 -0300

    criação e preparação do php na sidebar

---
+++ app/Controllers/SideController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class SideController
+{
+
+    public function index()
+    {
+        return view('admin/sidebar');
+    }
+}
+
+?>
--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,6 @@ namespace App\Controllers;
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
-    $router->get('', 'ExampleController@index');
+    $router->get('sidebar', 'SideController@index');
 
 ?>
