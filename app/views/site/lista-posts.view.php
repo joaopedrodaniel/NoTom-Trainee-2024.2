8bc9e26948e97cf8b6e9f791f7b268245ab63d7d
lucas <lucasferoliveira.goval@gmail.com>
2024-12-14 05:54:28 -0300

    Fixa o footer no pe da pagina

--- app/views/site/lista-posts.view.php
+++ app/views/site/lista-posts.view.php
@@ -1,62 +1,61 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/lista-posts.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-      rel="stylesheet"
-    />
-  </head>
 
-  <body>
- 
-    <div class="pagina-lista-posts">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="../../../public/css/lista-posts.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+    rel="stylesheet" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+    rel="stylesheet" />
+</head>
+
+<body>
+
+  
     <header><?php require 'header.php' ?></header>
-      <h1 class="titulo">Posts</h1>
-      <?php require 'componentes/Barra-de-pesquisas.php' ?>
-      <div class="conteudo-central">
-        <div class="lista-posts">
-          <?php foreach ($posts as $post): $user = App\Core\App::get('database')->select('usuarios', $post->id_autor)[0]; ?>
-            <div class="post">
-            <img src=<?=$post->imagem ?> alt="" />
+    <h1 class="titulo">Posts</h1>
+    <?php require 'componentes/Barra-de-pesquisas.php' ?>
+    <div class="conteudo-central">
+      <div class="lista-posts">
+        <?php foreach ($posts as $post): $user = App\Core\App::get('database')->select('usuarios', $post->id_autor)[0]; ?>
+          <div class="post">
+            <img src=<?= $post->imagem ?> alt="" />
             <div class="post-texto">
               <div class="post-conteudo">
-                <a href="/post-individual/<?=$post->id?>" class="post-titulo"><?=$post->titulo ?></a>
+                <a href="/post-individual/<?= $post->id ?>" class="post-titulo"><?= $post->titulo ?></a>
                 <div class="post-conteudo-texto">
-                  <?=$post->descricao?>
+                  <?= $post->descricao ?>
                 </div>
               </div>
-              <div class="post-conteudo-texto">Por: <?=$user->nome ?></div>
+              <div class="post-conteudo-texto">Por: <?= $user->nome ?></div>
             </div>
-            </div>
-          <?php endforeach; ?>
-          
-          
-          
-          
-        </div>
-        <?php require 'componentes/publicPostsPaginacao.php' ?>
+          </div>
+        <?php endforeach; ?>
+
+
+
+
       </div>
-      <footer class="teste1234"><?php require 'footer.php' ?></div>  
+      <?php require 'componentes/publicPostsPaginacao.php' ?>
     </div>
-    
-    <script
-      type="module"
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-    ></script>
-    <script src="../../../public/js/barra-busca.js"></script>
-  </body>
+    <footer class="teste1234"><?php require 'footer.php' ?>
+  </div>
+ 
+
+  <script
+    type="module"
+    src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script
+    nomodule
+    src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="../../../public/js/barra-busca.js"></script>
+</body>
+
 </html>
